8bd0c4db45f993cebd11bbabb1ce922d411d02fe
yhirsch <yeshayahirsch@gmail.com>
2022-04-28 17:40:07 +0300

    switch sides-turn 0

--- js/BoardData.js
+++ js/BoardData.js
@@ -8,10 +8,10 @@ class BoardData {
     this.pieces = [];
 
     for (let i = 0; i < BOARD_SIZE; i++) {
-      this.pieces.push(new Piece(0, i, PIECES[i], WHITE_PLAYER));
-      this.pieces.push(new Piece(1, i, PAWN, WHITE_PLAYER));
       this.pieces.push(new Piece(6, i, PAWN, BLACK_PLAYER));
       this.pieces.push(new Piece(7, i, PIECES[i], BLACK_PLAYER));
+      this.pieces.push(new Piece(0, i, PIECES[i], WHITE_PLAYER));
+      this.pieces.push(new Piece(1, i, PAWN, WHITE_PLAYER));
     }
   }
 
